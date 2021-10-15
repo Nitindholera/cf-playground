3f2ac46709e4f48cae5868bcd233caffb856259a
Nitindholera <nitin716sharma@gmail.com>
2021-10-15 17:39:33 +0530

    modiefied pizza forces

--- cf round 112 div 2/pizzaforces.cpp
+++ cf round 112 div 2/pizzaforces.cpp
@@ -5,19 +5,4 @@ typedef unsigned long long int lol;
 int main(){
     int t;
     cin>>t;
-    while(t--){
-        lol n;
-        cin>>n;
-        lol ans;
-        if(n<=6){
-            ans=3;
-        }
-        else if((n%2)==1){
-            ans=(lol)((n+1)/2);
-        }
-        else{
-            ans=n/2;
-        }
-        cout<<ans*5<<"\n";
-    }
 }
