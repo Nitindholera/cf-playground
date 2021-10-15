308ad57a8fb7e059fcf06e9fa3117c9f85116515
Nitindholera <nitin716sharma@gmail.com>
2021-10-15 17:56:52 +0530

    updated pizzaforces

--- cf round 112 div 2/pizzaforces.cpp
+++ cf round 112 div 2/pizzaforces.cpp
@@ -1,8 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
 typedef unsigned long long int lol;
-
+ 
 int main(){
     int t;
     cin>>t;
+    while(t--){
+        lol n;
+        cin>>n;
+        lol ans;
+        if(n<=6){
+            ans=3;
+        }
+        else if((n%2)==1){
+            ans=(lol)((n+1)/2);
+        }
+        else{
+            ans=n/2;
+        }
+        cout<<ans*5<<"\n";
+    }
 }
